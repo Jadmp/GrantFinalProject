cbcc12658a93ae2d9feaa0700200151bd6fc676a
matteepatty <39417063+matteepatty@users.noreply.github.com>
2019-05-21 09:57:14 -0700

    Matthew May 21st Update

---
+++ may21st.java
@@ -0,0 +1,381 @@
+
+public class StatesInAmerica {
+	int income = readInt("Please enter your annual income");
+	public String zeroException ()
+	{
+		if(income<=0)
+		{
+			return "You pay no taxes!";
+		}
+	}
+	double rate = 0.0 ;
+	String state= readLine(" Please Enter your state of residence");
+	
+	public boolean marriageStatus() {
+	String marriage;
+	boolean maritalStatus;
+
+	System.out.println("Are you currently married? If so, please enter 'y'. If not, please enter 'n'");
+	while (true) {
+	  marriage = in.nextLine().trim().toLowerCase();
+	  if (answer.equals("y")) {
+	    maritalStatus = true;
+	    break;
+	  } else if (answer.equals("n")) {
+	    maritalStatus = false;
+	    break;
+	  } else {
+	     System.out.println("Sorry, I didn't catch that. Please answer y/n");
+	  }
+	}
+	}
+	System.out.println(income + ", " + state);
+	
+	if(input.equals 'Alabama')
+	{
+		if(income<=500)
+		{
+			if(maritalStatus == true)
+			{
+				
+			}
+			else if(maritalStatus == false)
+			{
+			
+			
+		}
+	}
+	
+	if(input.equals 'Alaska')
+	{
+		
+	}
+	
+	if(input.equals 'Arizona')
+	{
+		
+	}
+	
+	if(input.equals 'Arkansas')
+	{
+		
+	}
+	
+	if(input.equals 'California')
+	{
+		
+	}
+	
+	if(input.equals 'Colorado')
+	{
+		
+	}
+	
+	if(input.equals 'Connecticut')
+	{
+		
+	}
+	
+	if(input.equals 'Delaware')
+	{
+		
+	}
+	
+	if(input.equals 'Florida')
+	{
+		
+	}
+	
+	if(input.equals 'Georgia')
+	{
+		
+	}
+	
+	if(input.equals 'Hawaii')
+	{
+		
+	}
+
+	if(input.equals 'Idaho')
+	{
+		
+	}
+	
+	if(input.equals 'Illinois')
+	{
+		
+	}
+	
+	if(input.equals 'Indiana;)
+	{
+		
+	}
+	
+	if(input.equals 'Iowa';)
+	{
+				
+	}
+	
+	if(input.equals 'Kansas';)
+	{
+				
+	}
+	
+	if(input.equals 'Kentucky';)
+	{
+				
+	}
+	
+	if(input.equals 'Louisiana';)
+	{
+				
+	}
+	
+	if(input.equals 'Maine';)
+	{
+				
+	}
+	
+	if(input.equals 'Maryland';)
+	{
+				
+	}
+	
+	if(input.equals 'Massachusetts';)
+	{
+				
+	}
+	
+	if(input.equals 'Michigan';)
+	{
+				
+	}
+	
+	if(input.equals 'Minnesota';)
+	{
+				
+	}
+	
+	if(input.equals 'Mississippi';)
+	{
+		
+	}
+	
+	if(input.equals 'Missouri';)
+	{
+		if(income<=99)
+		{
+			rate = 0.0;
+		}
+		else if(income >= 100 && income <= 1000)
+		{
+			rate = .015;
+		}
+		else if(income > 1000 && income <= 2000)
+		{
+			rate = .02;
+		}
+		else if(income > 2000 && income <= 3000)
+		{
+			rate = .025;
+		}
+		else if(income > 3000 && income <= 4000)
+		{
+			rate = .03;
+		}
+		else if(income > 4000 && income <= 5000)
+		{
+			rate = .035;
+		}
+		else if(income > 5000 && income <= 6000)
+		{
+			rate = .04;
+		}
+		else if(income > 6000 && income <= 7000)
+		{
+			rate = .045;
+		}
+		else if(income > 7000 && income <= 8000)
+		{
+			rate = .05;
+		}
+		else if(income > 8000 && income <= 9000)
+		{
+			rate = .055;
+		}
+		else if(income > 9000)
+		{
+			rate = .06;
+		}
+	}
+	
+	if(input.equals 'Montana';)
+	{
+		if(income <= 2800)
+		{
+			rate = .01;
+		}
+		else if(income > 2800 && income <= 5000)
+		{
+			rate = .02;
+		}
+		else if(income > 5000 && income <= 7600)
+		{
+			rate = .03;
+		}
+		else if(income > 7600 && income <= 10300)
+		{
+			rate = .04;
+		}
+		else if(income > 10300 && income <= 13300)
+		{
+			rate = .05;
+		}
+		else if(income > 13300 && income <= 17000)
+		{
+			rate = .06;
+		}
+		else if(income > 17000)
+		{
+			rate = .069;
+		}
+	}
+	
+	if(input.equals 'Nebraska';)
+	{
+		if(income <= 3050)
+		{
+			rate = .0246;
+		}
+		if(income > 3050 && income <= 18280)
+		{
+			rate = .0351;
+		}
+		if(income > 18280 && income <= 29460)
+		{
+			rate = .0501;
+		}
+		if(income > 29460)
+		{
+			rate = .0684;
+		}
+	}
+	
+	if(input.equals 'Nevada';)
+	{
+		rate = 0.0;
+	}
+	
+	if(input.equals 'New Hampshire';)
+	{
+		rate = .05;
+	}
+	
+	if(input.equals 'New Jersey';)
+	{
+				
+	}
+	
+	if(input.equals 'New Mexico';)
+	{
+				
+	}
+	
+	if(input.equals 'New York';)
+	{
+				
+	}
+	
+	if(input.equals 'North Carolina';)
+	{
+				
+	}
+	
+	if(input.equals 'North Dakota';)
+	{
+				
+	}
+	
+	if(input.equals 'Ohio';)
+	{
+				
+	}
+	
+	if(input.equals 'Oklahoma';)
+	{
+				
+	}
+	
+	if(input.equals 'Oregon';)
+	{
+				
+	}
+	
+	if(input.equals 'Pennsylvania';)
+	{
+				
+	}
+	
+	if(input.equals 'Rhode Island';)
+	{
+				
+	}
+	
+	if(input.equals 'South Carolina';)
+	{
+				
+	}
+	
+	if(input.equals 'South Dakota';)
+	{
+				
+	}
+	
+	if(input.equals 'Tennessee';)
+	{
+				
+	}
+	
+	if(input.equals 'Texas';)
+	{
+				
+	}
+	
+	if(input.equals 'Utah';)
+	{
+				
+	}
+	
+	if(input.equals 'Vermont';)
+	{
+				
+	}
+	
+	if(input.equals 'Virgina';)
+	{
+				
+	}
+	
+	if(input.equals 'Washington';)
+	{
+				
+	}
+	
+	if(input.equals 'West Virginia';)
+	{
+				
+	}
+	
+	if(input.equals 'Wisonsin';)
+	{
+				
+	}
+	
+	if(input.equals 'Wyoming';)
+	{
+				
+	}
+
+
+
+}

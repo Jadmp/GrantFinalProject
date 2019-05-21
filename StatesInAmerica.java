8237befb79734c3635780f34221e548ba14ad34a
Jadmp <50855863+Jadmp@users.noreply.github.com>
2019-05-20 20:27:03 -0700

    Class for all 50 states rough draft

---
+++ StatesInAmerica.java
@@ -0,0 +1,286 @@
+
+public class StatesInAmerica {
+	int income = readInt("Please enter your annual income");
+	public String zeroException ()
+	{
+		if(income<=0)
+		{
+			return "You pay no taxes!";
+		}
+	}
+	double rate = 0.0 ;
+	String state= readLine(" Please Enter your state of residence");
+	
+	public boolean marriageStatus() {
+	String marriage;
+	boolean martialStatus;
+
+	System.out.println("Are you currently married? If so, please enter 'y'. If not, please enter 'n'");
+	while (true) {
+	  marriage = in.nextLine().trim().toLowerCase();
+	  if (answer.equals("y")) {
+	    martialStatus = true;
+	    break;
+	  } else if (answer.equals("n")) {
+	    martialStatus = false;
+	    break;
+	  } else {
+	     System.out.println("Sorry, I didn't catch that. Please answer y/n");
+	  }
+	}
+	}
+	System.out.println(income + ", " + state);
+	
+	if(input.equals 'Alabama')
+	{
+		if(income<=500)
+	}
+	
+	if(input.equals 'Alaska')
+	{
+		
+	}
+	
+	if(input.equals 'Arizona')
+	{
+		
+	}
+	
+	if(input.equals 'Arkansas')
+	{
+		
+	}
+	
+	if(input.equals 'California')
+	{
+		
+	}
+	
+	if(input.equals 'Colorado')
+	{
+		
+	}
+	
+	if(input.equals 'Connecticut')
+	{
+		
+	}
+	
+	if(input.equals 'Delaware')
+	{
+		
+	}
+	
+	if(input.equals 'Florida')
+	{
+		
+	}
+	
+	if(input.equals 'Georgia')
+	{
+		
+	}
+	
+	if(input.equals 'Hawaii')
+	{
+		
+	}
+
+	if(input.equals 'Idaho')
+	{
+		
+	}
+	
+	if(input.equals 'Illinois')
+	{
+		
+	}
+	
+	if(input.equals 'Indiana;)
+	{
+		
+	}
+	
+	if(input.equals 'Iowa';)
+	{
+				
+	}
+	
+	if(input.equals 'Kansas';)
+	{
+				
+	}
+	
+	if(input.equals 'Kentucky';)
+	{
+				
+	}
+	
+	if(input.equals 'Louisiana';)
+	{
+				
+	}
+	
+	if(input.equals 'Maine';)
+	{
+				
+	}
+	
+	if(input.equals 'Maryland';)
+	{
+				
+	}
+	
+	if(input.equals 'Massachusetts';)
+	{
+				
+	}
+	
+	if(input.equals 'Michigan';)
+	{
+				
+	}
+	
+	if(input.equals 'Minnesota';)
+	{
+				
+	}
+	
+	if(input.equals 'Mississippi';)
+	{
+				
+	}
+	
+	if(input.equals 'Missouri';)
+	{
+				
+	}
+	
+	if(input.equals 'Montana';)
+	{
+				
+	}
+	
+	if(input.equals 'Nebraska';)
+	{
+				
+	}
+	
+	if(input.equals 'Nevada';)
+	{
+				
+	}
+	
+	if(input.equals 'New Hampshire';)
+	{
+				
+	}
+	
+	if(input.equals 'New Jersey';)
+	{
+				
+	}
+	
+	if(input.equals 'New Mexico';)
+	{
+				
+	}
+	
+	if(input.equals 'New York';)
+	{
+				
+	}
+	
+	if(input.equals 'North Carolina';)
+	{
+				
+	}
+	
+	if(input.equals 'North Dakota';)
+	{
+				
+	}
+	
+	if(input.equals 'Ohio';)
+	{
+				
+	}
+	
+	if(input.equals 'Oklahoma';)
+	{
+				
+	}
+	
+	if(input.equals 'Oregon';)
+	{
+				
+	}
+	
+	if(input.equals 'Pennsylvania';)
+	{
+				
+	}
+	
+	if(input.equals 'Rhode Island';)
+	{
+				
+	}
+	
+	if(input.equals 'South Carolina';)
+	{
+				
+	}
+	
+	if(input.equals 'South Dakota';)
+	{
+				
+	}
+	
+	if(input.equals 'Tennessee';)
+	{
+				
+	}
+	
+	if(input.equals 'Texas';)
+	{
+				
+	}
+	
+	if(input.equals 'Utah';)
+	{
+				
+	}
+	
+	if(input.equals 'Vermont';)
+	{
+				
+	}
+	
+	if(input.equals 'Virgina';)
+	{
+				
+	}
+	
+	if(input.equals 'Washington';)
+	{
+				
+	}
+	
+	if(input.equals 'West Virginia';)
+	{
+				
+	}
+	
+	if(input.equals 'Wisonsin';)
+	{
+				
+	}
+	
+	if(input.equals 'Wyoming';)
+	{
+				
+	}
+
+
+
+}
